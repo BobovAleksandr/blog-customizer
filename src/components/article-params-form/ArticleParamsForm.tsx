2e754d83155b56a69215fc589fcf572fde07dc37
Aleksandr Bobov <bobov.aleksandr.fr@yandex.ru>
2025-01-02 22:59:12 +0500

    Написал открытие панели и добавл компонент с выбором шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,49 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react';
+import { Select } from 'src/ui/select';
+import {
+	fontFamilyOptions,
+	defaultArticleState,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const asideClickHandler = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const [currentFont, setCurrentFont] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+
+	const selectChangeHandler = (currentFont: OptionType) => {
+		setCurrentFont(currentFont);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={asideClickHandler} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Select
+						selected={currentFont}
+						options={fontFamilyOptions}
+						placeholder={defaultArticleState.fontFamilyOption.title}
+						onChange={(selectedValue) => {
+							selectChangeHandler(selectedValue);
+						}}
+						onClose={() => {
+							console.log('close');
+						}}
+						title='Шрифт'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
