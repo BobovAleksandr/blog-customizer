052b9b09aeb82bba788cb4425237a8ea259617d8
Aleksandr Bobov <bobov.aleksandr.fr@yandex.ru>
2025-01-03 22:58:16 +0500

    Дописал остальыне компоненты

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,17 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { useState } from 'react';
 import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
 import {
 	fontFamilyOptions,
 	defaultArticleState,
 	OptionType,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
 } from 'src/constants/articleProps';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
@@ -22,8 +28,48 @@ export const ArticleParamsForm = () => {
 		defaultArticleState.fontFamilyOption
 	);
 
-	const selectChangeHandler = (currentFont: OptionType) => {
+	const fontChangeHandler = (currentFont: OptionType) => {
 		setCurrentFont(currentFont);
+	};
+
+	const [currentFontSize, setCurrentFontSize] = useState(
+		defaultArticleState.fontSizeOption
+	);
+
+	const RadioGroupChangeHandler = (currentFontSize: OptionType) => {
+		setCurrentFontSize(currentFontSize);
+	};
+
+	const [currentFontColor, setCurrentFontColor] = useState(
+		defaultArticleState.fontColor
+	);
+
+	const fontColorChangeHandler = (currentFontColor: OptionType) => {
+		setCurrentFontColor(currentFontColor);
+	};
+
+	const [currentBackgroundColor, setCurrentBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+
+	const backgroundColorChangeHandler = (currentBackgroundColor: OptionType) => {
+		setCurrentBackgroundColor(currentBackgroundColor);
+	};
+
+	const [currentContentWidth, setCurrentContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	const contentWidthChangeHandler = (currentContentWidth: OptionType) => {
+		setCurrentContentWidth(currentContentWidth);
+	};
+
+	const resetHandler = () => {
+		setCurrentFont(defaultArticleState.fontFamilyOption);
+		setCurrentFontSize(defaultArticleState.fontSizeOption);
+		setCurrentFontColor(defaultArticleState.fontColor);
+		setCurrentBackgroundColor(defaultArticleState.backgroundColor);
+		setCurrentContentWidth(defaultArticleState.contentWidth);
 	};
 
 	return (
@@ -37,16 +83,70 @@ export const ArticleParamsForm = () => {
 						options={fontFamilyOptions}
 						placeholder={defaultArticleState.fontFamilyOption.title}
 						onChange={(selectedValue) => {
-							selectChangeHandler(selectedValue);
+							fontChangeHandler(selectedValue);
 						}}
 						onClose={() => {
-							console.log('close');
+							console.log('close'); // TODO что тут должно быть?
 						}}
 						title='Шрифт'
 					/>
+					<RadioGroup
+						name='Размер шрифта'
+						options={fontSizeOptions}
+						selected={currentFontSize}
+						onChange={RadioGroupChangeHandler}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={currentFontColor}
+						options={fontColors}
+						placeholder={defaultArticleState.fontColor.title}
+						onChange={(selectedValue) => {
+							fontColorChangeHandler(selectedValue);
+						}}
+						onClose={() => {
+							console.log('close'); // TODO что тут должно быть?
+						}}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={currentBackgroundColor}
+						options={backgroundColors}
+						placeholder={defaultArticleState.backgroundColor.title}
+						onChange={(selectedValue) => {
+							backgroundColorChangeHandler(selectedValue);
+						}}
+						onClose={() => {
+							console.log('close'); // TODO что тут должно быть?
+						}}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={currentContentWidth}
+						options={contentWidthArr}
+						placeholder={defaultArticleState.contentWidth.title}
+						onChange={(selectedValue) => {
+							contentWidthChangeHandler(selectedValue);
+						}}
+						onClose={() => {
+							console.log('close'); // TODO что тут должно быть?
+						}}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={resetHandler}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={submitHandler}
+						/>
 					</div>
 				</form>
 			</aside>
